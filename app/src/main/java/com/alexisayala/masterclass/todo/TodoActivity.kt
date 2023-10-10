f2600b0eb2a21b5accf5961a09b61dda355b31ba
Alexis Ayala <lex.solorio15@gmail.com>
2023-10-10 16:02:17 -0600

    (feat): added dialog add funcion

--- app/src/main/java/com/alexisayala/masterclass/todo/TodoActivity.kt
+++ app/src/main/java/com/alexisayala/masterclass/todo/TodoActivity.kt
@@ -1,7 +1,12 @@
 package com.alexisayala.masterclass.todo
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.alexisayala.masterclass.R
@@ -26,6 +31,7 @@ class TodoActivity : AppCompatActivity() {
 
     private lateinit var fabAddTask: FloatingActionButton
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_todo)
@@ -39,6 +45,7 @@ class TodoActivity : AppCompatActivity() {
         recicleViewCategories = findViewById(R.id.rvCategories)
         rvTasks = findViewById(R.id.rvTasks)
         fabAddTask = findViewById(R.id.fabAddTask)
+
     }
 
     private fun initUI() {
@@ -56,10 +63,38 @@ class TodoActivity : AppCompatActivity() {
         fabAddTask.setOnClickListener {
             showDialog()
         }
+
     }
 
 
     private fun showDialog() {
+        val dialog = Dialog(this)
+        dialog.setContentView(R.layout.dialog_task)
 
+        val btAddTask: Button = dialog.findViewById(R.id.btAddTask)
+        val edTask: EditText = dialog.findViewById(R.id.edTask)
+        val rgCategories: RadioGroup = dialog.findViewById(R.id.rgCategories)
+
+        btAddTask.setOnClickListener {
+            val currentTask = edTask.text.toString()
+            if (currentTask.isNotEmpty()) {
+                val selectedId = rgCategories.checkedRadioButtonId
+                val selectedRadioButton: RadioButton = rgCategories.findViewById(selectedId)
+                val currentCategory: TaskCategories = when (selectedRadioButton.text) {
+                    getString(R.string.dialog_business) -> Business
+                    getString(R.string.dialog_personal) -> Personal
+                    else -> Other
+                }
+                tasks.add(Tasks(currentTask, currentCategory))
+                updateTask()
+                dialog.hide()
+            }
+        }
+
+        dialog.show()
+    }
+
+    private fun updateTask() {
+        tasksAdapter.notifyDataSetChanged()
     }
 }
