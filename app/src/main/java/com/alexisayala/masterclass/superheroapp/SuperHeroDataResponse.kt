7e050d272d95529ee99eb36f4c83d2b869243237
Alexis Ayala <lex.solorio15@gmail.com>
2023-10-22 17:42:00 -0600

    (feature): get api rest from superhero endpoint getting result

--- app/src/main/java/com/alexisayala/masterclass/superheroapp/SuperHeroDataResponse.kt
+++ app/src/main/java/com/alexisayala/masterclass/superheroapp/SuperHeroDataResponse.kt
@@ -2,5 +2,13 @@ package com.alexisayala.masterclass.superheroapp
 
 import com.google.gson.annotations.SerializedName
 
-data class SuperHeroDataResponse(@SerializedName("response") val response: String) {
+data class SuperHeroDataResponse(
+    @SerializedName("response") val response: String,
+    @SerializedName("results") val superHeroes: List<SuperHeroesItemsResponse>
+) {
 }
+
+data class SuperHeroesItemsResponse(
+    @SerializedName("id") val superHeroId: String,
+    @SerializedName("name") val superHeroName: String
+)
